fd5668608f67289c4ba4ffde2a9d2d876f400e2b
Kareem Grant <kareem@getuserwise.com>
2014-07-23 18:26:53 -0400

    Replace new action buttons with an select dropdown that is more scalable for adding additional action types.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def format_action_type(instance)
     instance.class.name.gsub("Action", "")
   end
+
+  def action_type_collection
+    [["Scavenger", "scavenger"], ["Simpleshare", "simpleshare"]]
+  end
+
 end
